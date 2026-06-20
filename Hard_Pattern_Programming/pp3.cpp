5b50b8da7450b2b59675815c95ecf2a6399bdee0
Ritesh Vishwakarma <113338495+RiteshhVishwakarma@users.noreply.github.com>
2026-06-20 23:06:31 +0530

    pattern programming practice

--- Hard_Pattern_Programming/pp3.cpp
+++ Hard_Pattern_Programming/pp3.cpp
@@ -1,19 +1,13 @@
-// 1 
-// 1 2 
-// 1 2 3 
-// 1 2 3 4 
-// 1 2 3 4 5 
-
-
 #include <iostream> 
 using namespace std;
-int main(){
+int main()
+{
     int row, col;
     for(row=1; row<=5; row++)
     {
         for(col=1; col<=row; col++)
         {
-            cout<<col<<" ";
+            cout<<row<<" ";
         }
         cout<<endl;
     }
